0ae9d92b23cb60e36f52d3d489fdd77dfa07b637
adam <adamcalculator272@gmail.com>
2024-03-18 23:34:39 +0700

    1.19.4 backport

--- src/client/java/com/adamcalculator/dynamicpack/DebugScreen.java
+++ src/client/java/com/adamcalculator/dynamicpack/DebugScreen.java
@@ -1,15 +1,8 @@
 package com.adamcalculator.dynamicpack;
 
 
-import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import com.adamcalculator.dynamicpack.pack.Pack;
-import com.adamcalculator.dynamicpack.util.Out;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.toast.SystemToast;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 
