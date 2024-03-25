4e8f0b3ca1a51e6eb21160a842b970cfb3f67499
adam <adamcalculator272@gmail.com>
2024-03-25 11:46:01 +0300

    bump version

--- common/src/main/java/com/adamcalculator/dynamicpack/Mod.java
+++ common/src/main/java/com/adamcalculator/dynamicpack/Mod.java
@@ -8,8 +8,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Mod {
-    public static final long VERSION_BUILD = 18;
-    public static final String VERSION_NAME_MOD = "1.0.18";
+    public static final long VERSION_BUILD = 19;
+    public static final String VERSION_NAME_MOD = "1.0.19";
     public static final String VERSION_NAME_BRANCH = "mc1.20.4";
     public static final String VERSION_NAME =  VERSION_NAME_MOD + "+" + VERSION_NAME_BRANCH;
     public static final String MOD_ID = "dynamicpack";
