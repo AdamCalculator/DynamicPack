5986e585ace9572e92e3c7295ed5b98e4b601bb7
adam <adamcalculator272@gmail.com>
2024-03-25 14:37:19 +0300

    fix breaking paths in logs and small improve in security

--- common/src/main/java/com/adamcalculator/dynamicpack/InputValidator.java
+++ common/src/main/java/com/adamcalculator/dynamicpack/InputValidator.java
@@ -1,10 +1,12 @@
 package com.adamcalculator.dynamicpack;
 
+import java.nio.charset.StandardCharsets;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class InputValidator {
     private static final Pattern CONTENT_ID_PATTERN = Pattern.compile("^[a-z0-9_:-]{2,128}$");
+    private static final Pattern PATH_PATTERN = Pattern.compile("^[A-Za-z0-9_./() +-]{0,255}$");
 
     public static boolean isContentIdValid(String input) {
         if (input == null) {
@@ -28,5 +30,18 @@ public class InputValidator {
         }
 
         return input.trim().length() < 64 && !input.trim().isEmpty() && !input.contains("\n") && !input.contains("\r") && !input.contains("\b");
+    }
+
+    public static boolean isHashValid(String hash) {
+        return hash != null && hash.length() == 40 && !hash.contains(" ");
+    }
+
+    public static void validOrThrownPath(String par) {
+        if (par == null) {
+            throw new SecurityException("null", new NullPointerException("path to valid is null"));
+        }
+        if (!PATH_PATTERN.matcher(par).matches()) {
+            throw new SecurityException("Not valid path: " + new String(par.getBytes(StandardCharsets.US_ASCII), StandardCharsets.US_ASCII));
+        }
     }
 }
